3da429f76c6b71250a0d46f92737ebcadbb3ac6a
keanu <keonk87@icloud.com>
2022-04-15 23:04:56 -0400

    Updated UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,13 +44,14 @@ namespace File_Compare
             // 
             // addFile1Button
             // 
-            this.addFile1Button.BackColor = System.Drawing.Color.LightCoral;
+            this.addFile1Button.BackColor = System.Drawing.Color.DarkOrchid;
             this.addFile1Button.FlatAppearance.BorderSize = 0;
             this.addFile1Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.addFile1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.addFile1Button.Location = new System.Drawing.Point(12, 131);
+            this.addFile1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addFile1Button.ForeColor = System.Drawing.SystemColors.Control;
+            this.addFile1Button.Location = new System.Drawing.Point(12, 105);
             this.addFile1Button.Name = "addFile1Button";
-            this.addFile1Button.Size = new System.Drawing.Size(171, 63);
+            this.addFile1Button.Size = new System.Drawing.Size(80, 21);
             this.addFile1Button.TabIndex = 0;
             this.addFile1Button.Text = "Add File 1";
             this.addFile1Button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -59,21 +60,22 @@ namespace File_Compare
             // 
             // file1TextBox
             // 
-            this.file1TextBox.Location = new System.Drawing.Point(12, 105);
+            this.file1TextBox.Location = new System.Drawing.Point(98, 105);
             this.file1TextBox.Name = "file1TextBox";
             this.file1TextBox.ReadOnly = true;
-            this.file1TextBox.Size = new System.Drawing.Size(171, 20);
+            this.file1TextBox.Size = new System.Drawing.Size(204, 20);
             this.file1TextBox.TabIndex = 1;
             // 
             // addFile2Button
             // 
-            this.addFile2Button.BackColor = System.Drawing.Color.LightCoral;
+            this.addFile2Button.BackColor = System.Drawing.Color.DarkOrchid;
             this.addFile2Button.FlatAppearance.BorderSize = 0;
             this.addFile2Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.addFile2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.addFile2Button.Location = new System.Drawing.Point(260, 131);
+            this.addFile2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addFile2Button.ForeColor = System.Drawing.SystemColors.Control;
+            this.addFile2Button.Location = new System.Drawing.Point(12, 156);
             this.addFile2Button.Name = "addFile2Button";
-            this.addFile2Button.Size = new System.Drawing.Size(171, 63);
+            this.addFile2Button.Size = new System.Drawing.Size(80, 20);
             this.addFile2Button.TabIndex = 2;
             this.addFile2Button.Text = "Add File 2";
             this.addFile2Button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -82,31 +84,34 @@ namespace File_Compare
             // 
             // file2TextBox
             // 
-            this.file2TextBox.Location = new System.Drawing.Point(260, 105);
+            this.file2TextBox.Location = new System.Drawing.Point(98, 156);
             this.file2TextBox.Name = "file2TextBox";
             this.file2TextBox.ReadOnly = true;
-            this.file2TextBox.Size = new System.Drawing.Size(171, 20);
+            this.file2TextBox.Size = new System.Drawing.Size(204, 20);
             this.file2TextBox.TabIndex = 3;
             // 
             // Title
             // 
             this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.Transparent;
             this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.Title.ForeColor = System.Drawing.SystemColors.Control;
             this.Title.Location = new System.Drawing.Point(12, 9);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(222, 39);
+            this.Title.Size = new System.Drawing.Size(298, 39);
             this.Title.TabIndex = 4;
-            this.Title.Text = "File Compare";
+            this.Title.Text = "File Compare Tool";
             // 
             // compareButton
             // 
-            this.compareButton.BackColor = System.Drawing.Color.RosyBrown;
+            this.compareButton.BackColor = System.Drawing.Color.DarkOrchid;
             this.compareButton.FlatAppearance.BorderSize = 0;
             this.compareButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.compareButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.compareButton.Location = new System.Drawing.Point(260, 238);
+            this.compareButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compareButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.compareButton.Location = new System.Drawing.Point(228, 192);
             this.compareButton.Name = "compareButton";
-            this.compareButton.Size = new System.Drawing.Size(171, 69);
+            this.compareButton.Size = new System.Drawing.Size(74, 34);
             this.compareButton.TabIndex = 5;
             this.compareButton.Text = "Compare";
             this.compareButton.UseVisualStyleBackColor = false;
@@ -116,8 +121,8 @@ namespace File_Compare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(455, 319);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(316, 261);
             this.Controls.Add(this.compareButton);
             this.Controls.Add(this.Title);
             this.Controls.Add(this.file2TextBox);
@@ -126,7 +131,8 @@ namespace File_Compare
             this.Controls.Add(this.addFile1Button);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "File Compare Tool";
             this.ResumeLayout(false);
             this.PerformLayout();
 
